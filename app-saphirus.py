a5b579af0735e900fe0fcda647cf14e94bb5863f
gabi-2021 <93274203+gabi-2021@users.noreply.github.com>
2026-01-03 02:30:27 -0300

    Version 46.4 app-saphirus.py

--- app-saphirus.py
+++ app-saphirus.py
@@ -422,6 +422,7 @@ with tab3:
                     edited_df_cat = st.data_editor(
                         df_cat,
                         column_config={
+                            "_index": st.column_config.Column(hidden=True),
                             "Estado": st.column_config.SelectboxColumn(
                                 "Est",
                                 options=["pdte.", "ped.", "rep."],
@@ -504,3 +505,4 @@ with tab5:
              for k,v in dif.items(): st.write(f"**{k}**: {v[0]} -> {v[1]}")
 
 st.caption("Modo Offline Seguro - v46")
+
